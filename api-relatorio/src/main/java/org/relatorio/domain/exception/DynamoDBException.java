d9b783f5b29661deefdd26739676fe65db48896c
Dev <diogo.vds@hotmail.com>
2026-07-28 22:14:18 -0300

    versão final do projeto

--- api-relatorio/src/main/java/org/relatorio/domain/exception/DynamoDBException.java
+++
@@ -1,7 +0,0 @@
-package org.relatorio.domain.exception;
-
-public class DynamoDBException extends RuntimeException {
-    public DynamoDBException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
