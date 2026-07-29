96aad4f109eb67993f0bdc340e14b9be9ca00060
Dev <diogo.vds@hotmail.com>
2026-07-28 22:01:21 -0300

    versão final do projeto

--- api-avaliacao/README.md
+++
@@ -1,2 +0,0 @@
-Boas-vindas ao Tech Challenge da fase 4!
-Esta é a APi de notificação que recebe os dados da avaliação e envia uma notificação para os responsáveis
--- api-avaliacao/src/main/java/com/postech/techchallenge/fase4/avaliacao/dto/AvaliacaoResponseRecord.java
+++
@@ -1,5 +0,0 @@
-package com.postech.techchallenge.fase4.avaliacao.dto;
-
-public record AvaliacaoResponseRecord (String mensagem,
-                                       String id){
-}
